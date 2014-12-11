650257b410901512702157f86b1e64b0afba5018
William Manley <will@williammanley.net>
2014-12-11 02:17:54 +0000

    Add a few more example testcases

--- tests/example_test.py
+++ tests/example_test.py
@@ -1,7 +1,21 @@
-import stbt
 from time import sleep
+
+import stbt
+
+
+def test_that_live_tv_is_playing():
+    stbt.press('KEY_CLOSE')  # Close any open menus
+    assert stbt.wait_for_motion()
+
 
 def test_that_stb_tester_logo_is_shown():
     stbt.press('KEY_CHANNELUP')
+    assert stbt.wait_for_match('stb-tester-logo.png')
+
+
+def test_read_menu():
+    stbt.press('KEY_CLOSE')
     sleep(1)
-    assert stbt.match('stb-tester-logo.png')
+    stbt.press('KEY_MENU')
+    sleep(1)
+    print stbt.ocr()
