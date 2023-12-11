f9432ce0673659d566f32ccea4a235aaa8a00c23
David Röthlisberger <david@stb-tester.com>
2023-12-11 12:45:04 +0000

    Upgrade stbt_rig.py to latest version
    From <https://github.com/stb-tester/stbt-rig> as of commit b314dc0.
    
    New features:
    
    - "auth logout" command to remove your access token from the system
      keyring.
    
    Fixes to the VSCode integration:
    
    - Update pylint configuration so it doesn't use deprecated configuration
      settings.
    
    Other fixes:
    
    - "snapshot" command times out and fails with a useful error message if
      hung waiting for a different "stbt_rig.py snapshot" process to
      complete.

--- stbt_rig.py
+++ stbt_rig.py
@@ -113,6 +113,10 @@ def main_with_args(args):
     # Do this before importing requests, we will be installing requests in here:
     if args.command == "setup":
         return cmd_setup(args, args.node_id)
+    elif args.command == "auth":
+        # auth logout doesn't require a portal - and we don't want to prompt
+        # for an auth token just before deleting it!
+        return cmd_auth(args)
 
     portal = Portal.from_args(args)
     node = Node(portal, args.node_id)
@@ -363,6 +367,15 @@ def argparser():
         "name", help="Name that can be used to retrieve the secret")
     encrypt_secret_parser.add_argument("value", help="Value to be encrypted")
 
+    auth_parser = subcommands.add_parser(
+        "auth", help="Authenticate with the Stb-tester Portal")
+    auth_subparser = auth_parser.add_subparsers(dest="auth_command")
+    auth_subparser.add_parser(
+        "get-username",
+        help="Prints the GitHub username of the authenticated user")
+    auth_subparser.add_parser(
+        "logout", help="Delete the auth token from your keyring on this PC")
+
     screenshot_parser = subcommands.add_parser(
         "screenshot", help="Save a screenshot to disk",
         description="""Take a screenshot from the specified Stb-tester node
@@ -395,6 +408,30 @@ def _exit(signo, _):
     logger.warning("Received %s. Stopping job.", name)
     # Teardown is handled by TestJob.__exit__
     sys.exit(0)
+
+
+def cmd_auth(args):
+    # type: (argparse.Namespace) -> int
+    if args.auth_command == "logout":
+        try:
+            import keyring
+            if keyring.get_password(args.portal_url, "stb-tester") is None:
+                sys.stderr.write("No auth token stored in keyring\n")
+            else:
+                keyring.delete_password(args.portal_url, "stb-tester")
+                sys.stderr.write("Deleted auth token from keyring\n")
+        except ImportError:
+            sys.stderr.write(
+                "Install the python \"keyring\" package to use "
+                "\"stbt_rig auth\"\n")
+            return 1
+    elif args.auth_command == "get-username" or args.auth_command is None:
+        # This will prompt the user for a token if none is stored:
+        portal = Portal.from_args(args)
+        print(portal.get_user()['login'])
+    else:
+        assert False, "Unreachable: Unknown auth_command %r" % args.auth_command
+    return 0
 
 
 def cmd_run(args, node):
@@ -490,6 +527,7 @@ def cmd_run_prep(args, portal):
 
 
 def cmd_run_body(args, node, j):
+    # type: (argparse.Namespace, Node, JobPrepResult) -> int
     logger.info("Running tests...")
 
     try:
@@ -771,7 +809,7 @@ def setup_stage2(this_stbt_rig, root, args, node_id):
 
     # This will prompt for the auth token and validate connectivity:
     portal = Portal.from_args(args)
-    portal._get("/api/v2/user")
+    portal.get_user()
 
     for k in "name", "email":
         try:
@@ -867,12 +905,10 @@ def _update_vscode_config():
         "git.fetchOnPull": True,
         "git.pruneOnFetch": True,
         "git.pullBeforeCheckout": True,
+        "pylint.args": ["--load-plugins=_stbt.pylint_plugin"],
+        "pylint.path": ["pylint"],
         "python.envFile": "${workspaceFolder}/.env",
-        "python.linting.enabled": True,
-        "python.linting.mypyEnabled": False,
         "python.testing.nosetestsEnabled": False,
-        "python.linting.pylintArgs": ["--load-plugins=_stbt.pylint_plugin"],
-        "python.linting.pylintEnabled": True,
         "python.testing.pytestArgs": [
             "-p", "stbt_rig",
             "-p", "no:python",
@@ -896,6 +932,13 @@ def _update_vscode_config():
             }
         ]
     }
+    DEPRECATED_KEYS = {
+        "python.linting.enabled",
+        "python.linting.mypyEnabled",
+        "python.linting.pylintArgs",
+        "python.linting.pylintEnabled",
+    }
+
     try:
         with open(".vscode/settings.json") as f:
             cfg = json.load(f)
@@ -906,6 +949,10 @@ def _update_vscode_config():
     for k, v in VS_CODE_CONFIG.items():
         if cfg.get(k) != v:
             cfg[k] = v
+            modified = True
+    for k in list(cfg.keys()):
+        if k in DEPRECATED_KEYS:
+            del cfg[k]
             modified = True
 
     if modified:
@@ -942,9 +989,7 @@ def _update_env(updates):
 
 
 def _get_snapshot_branch_name(portal):
-    response = portal._get("/api/v2/user")
-    response.raise_for_status()
-    username = response.json()["login"]
+    username = portal.get_user()["login"]
     return "refs/snapshots/%s" % username
 
 
@@ -1060,7 +1105,9 @@ class PortalAuthTokensAdapter(HTTPAdapter):
                    self.portal_url,))
 
         while True:
-            token = ask('Enter Access Token for portal %s: ' % self.portal_url)
+            sys.stderr.write(
+                'Enter Access Token for portal %s: ' % self.portal_url)
+            token = ask()
             if not token:
                 continue
             token = token.strip()
@@ -1346,8 +1393,8 @@ class TimeoutException(RuntimeError):
 
 class Node(object):
     def __init__(self, portal, node_id):
-        self.portal = portal
-        self.node_id = node_id
+        self.portal = portal  # type: Portal
+        self.node_id = node_id  # type: str
 
     def run_tests(self, *args, **kwargs):
         return self.portal.run_tests(self.node_id, *args, **kwargs)
@@ -1490,6 +1537,11 @@ class Portal(object):
         resp.raise_for_status()
         return resp.text
 
+    def get_user(self):
+        resp = self._get("/api/v2/user")
+        resp.raise_for_status()
+        return resp.json()
+
     def _get(self, endpoint, timeout=60, **kwargs):
         return self._session.get(self.url(endpoint), timeout=timeout, **kwargs)
 
@@ -1615,7 +1667,8 @@ class TestPack(object):
         # otherwise github can error with "rejected", so use file locking:
         lockfilename = self._git(
             ["rev-parse", "--git-path", "stbt-rig-snapshot.lock"]).strip()
-        with open(lockfilename, "w") as lock, file_lock(lock.fileno()):
+
+        with flock(lockfilename):
             commit_sha, run_sha = self.take_snapshot()
             options = ['--force']
             if not logger.isEnabledFor(logging.DEBUG):
@@ -1630,20 +1683,69 @@ class TestPack(object):
             return run_sha
 
 
+@contextmanager
+def flock(filename):
+    with open(filename, "a+b") as f:
+        f.seek(0)
+        failed_attempting_lock = True
+        try:
+            with file_lock(f.fileno()):
+                failed_attempting_lock = False
+                bytes_written = f.write(b"%20d" % os.getpid())
+                f.flush()
+                f.truncate(bytes_written)
+                yield
+        except (OSError, IOError) as e:
+            if failed_attempting_lock:
+                msg = "Failed to lock %r: %s" % (filename, e)
+                if platform.system() != "Windows":
+                    try:
+                        pid = to_native_str(f.read(20).strip())
+                        msg += ".  File is currently locked by pid %s" % pid
+                    except Exception:  # pylint:disable=broad-except
+                        logger.warning(
+                            "Failed to read pid from %r", filename,
+                            exc_info=True)
+                die(msg)
+            else:
+                raise
+
+
 if platform.system() == "Windows":
     @contextmanager
     def file_lock(fileno):
+        # Arbitrarily chosen position in the file, sufficiently far from the
+        # beginning that it won't overlap with the pid:
+        LOCK_POS = 20
+
         import msvcrt  # pylint: disable=import-error
+        # Seek to specific position and lock 1 byte.  We don't want to lock the
+        # beginning of the file because that would prevent other processes from
+        # reading the pid from it:
+        pos = os.lseek(fileno, LOCK_POS, os.SEEK_SET)
         msvcrt.locking(fileno, msvcrt.LK_LOCK, 1)
+        os.lseek(fileno, pos, os.SEEK_SET)
         try:
             yield
         finally:
+            pos = os.lseek(fileno, LOCK_POS, os.SEEK_SET)
             msvcrt.locking(fileno, msvcrt.LK_UNLCK, 1)
+            os.lseek(fileno, pos, os.SEEK_SET)
 else:
     @contextmanager
     def file_lock(fileno):
         import fcntl
-        fcntl.flock(fileno, fcntl.LOCK_EX)
+        # Makes locking on UNIX match the behaviour on Windows where we give up
+        # on locking after 10s:
+        for n in range(10):
+            try:
+                fcntl.flock(fileno, fcntl.LOCK_EX | fcntl.LOCK_NB)
+                break
+            except IOError as e:
+                if e.errno in [errno.EAGAIN, errno.EWOULDBLOCK] and n < 9:
+                    time.sleep(1)
+                else:
+                    raise
         try:
             yield
         finally:
